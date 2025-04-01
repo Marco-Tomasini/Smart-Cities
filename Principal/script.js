0c9beeda64b25695a5d2acaebac6295845e13cff
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-04-01 12:55:53 -0300

    UPDATE - Ciclo de troca de senha e login completo

--- Principal/script.js
+++ Principal/script.js
@@ -1,4 +1,4 @@
-function validarDados(){
+function validarDadosLogin(){
     const email = document.getElementById('email').value.trim();
     const senha = document.getElementById('senha').value.trim();
 
@@ -16,5 +16,40 @@ function validarDados(){
     }
 
     window.location.href = "Dashboard.html"
+    return false
+}
+
+function validarDadosEmail(){
+    const emailrec = document.getElementById('emailrec').value.trim();
+
+    console.log(emailrec);
+
+    if(!emailrec){
+        alert('Por favor, insira seu E-mail');
+        return false;
+    }
+
+    window.location.href = "novasenha.html"
+    return false
+}
+
+function validarDadosSenha(){
+    const novaSenha = document.getElementById('novaSenha').value.trim();
+    const confirmSenha = document.getElementById('confirmSenha').value.trim();
+
+    console.log(novaSenha);
+    console.log(confirmSenha);
+
+    if(!novaSenha || novaSenha.length < 8){
+        alert('Por favor, insira uma senha que tenha pelo menos 8 dígitos');
+        return false;
+    }
+
+    if(!confirmSenha || confirmSenha !== novaSenha){
+        alert('Por favor, insira a mesma senha');
+        return false;
+    }
+
+    window.location.href = "login.html"
     return false
 }
