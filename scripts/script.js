f78c338e4367dc69c46de0728d8aa375106c4084
Carlos-Bittencourt <carlos_bittencourt@estudante.sesisenai.org.br>
2025-04-25 12:35:12 -0300

    UPDATE - Sidebar pronta

--- scripts/script.js
+++ scripts/script.js
@@ -56,8 +56,10 @@ function validarDadosSenha(){
 
 function openSidebar(){
     document.getElementById("sidebar").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
 function closeSidebar(){
     document.getElementById("sidebar").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
